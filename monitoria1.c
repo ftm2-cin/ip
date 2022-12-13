d3963ebcd7dd775aa3ad909845f864aa2b643549
ftm2-cin <ftm2@cin.ufpe.br>
2022-12-13 16:08:20 -0300

    COMMIT

--- monitoria1.c
+++ monitoria1.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     float num1, num2, soma, sub, mult, div;
     char operador;
-    
+
     printf("Digite dois numeros: ");
     scanf("%f %f", &num1, &num2);
-    
+
     printf("Digite uma operacao: + - * /: \n");
     scanf(" %c", &operador);
 
@@ -14,20 +15,20 @@ int main(){
     {
     case '+':
         soma = num1 + num2;
-        printf("A soma desses numeros e %.0f\n", soma);
+        printf("A soma desses numeros e %.2f\n", soma);
         break;
     case '-':
         sub = num1 - num2;
-        printf("A subtracao desses dois numeros e %.0f\n", sub);
+        printf("A subtracao desses dois numeros e %.2f\n", sub);
         break;
     case '*':
         mult = num1 * num2;
-        printf("A multiplicacao desses dois numeros e %.0f\n", mult);
+        printf("A multiplicacao desses dois numeros e %.2f\n", mult);
         break;
     case '/':
         div = num1 / num2;
-        printf("A divisao desses dois numeros e %.0f\n", div);
-        break;   
+        printf("A divisao desses dois numeros e %.2f\n", div);
+        break;
     default:
         printf("Voce nao escolheu uma opcao valida.");
         break;
