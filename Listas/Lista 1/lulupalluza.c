64bf8b806e375d34ac0ed0c80ea35ea281ae1042
ftm2-cin <ftm2@cin.ufpe.br>
2022-12-18 02:54:57 -0300

    atualizaçao lullapaluza

--- Listas/Lista 1/lulupalluza.c
+++ Listas/Lista 1/lulupalluza.c
@@ -8,7 +8,7 @@ int main()
     int a, c, m, qt1, qt2, qt3, qt4, v1, v2, v3, v4, resto1, resto2;
     scanf("%d %d %d", &a, &c, &m);
 
-    if (a >= 1 && a <= 100000  && c >= 1 && c <= 100000 && m >= 1 && m <= 20)
+    if (a >= 1 && a <= 100000 && c >= 1 && c <= 100000 && m >= 1 && m <= 20)
     {
         if (a % m == 0 && c % m == 0)
         {
@@ -35,7 +35,7 @@ int main()
                 printf("%d mesas com criancas: R$%d\n", qt2, v2);
                 printf("%d mesas mistas: R$%d\n", qt3, v3);
             }
-            else if (resto1 % 2 == 0 && resto2 % 2 == 0)
+            else if (resto1 % 2 == 0 && resto2 % 2 == 0 && (resto1 + resto2) != resto1 && (resto1 + resto2) != resto2)
             {
                 qt1 = ((a - a % m) / m);
                 qt2 = ((c - c % m) / m);
@@ -46,20 +46,37 @@ int main()
                 printf("%d mesas com adultos: R$%d\n", qt1, v1);
                 printf("%d mesas com criancas: R$%d\n", qt2, v2);
                 printf("%d mesas mistas: R$%d\n", qt3, v3);
-            }else if ((resto1 + resto2) >= 1 && (resto1 + resto2) <= 4)
+            }
+            else if ((resto1 + resto2) >= 1 && (resto1 + resto2) <= 4)
             {
-                qt1 = (((a - a % m) / m) - qt3);
-                qt2 = (((c - c % m) / m) - qt4);
+                qt1 = ((a - a % m) / m) - resto1;
+                qt2 = ((c - c % m) / m) - resto2;
                 qt3 = (resto1);
                 qt4 = (resto2);
                 v1 = qt1 * (3 * m);
                 v2 = qt2 * (2 * m);
-                v3 = qt3 * (4 * m);
-                v3 = qt4 * (3 * m);
+                v3 = ((qt3 * (4 * m)) + (resto1 * 4));
+                v4 = ((qt4 * (3 * m)) + (resto2 * 3));
+                printf("%d mesas com adultos: R$%d\n", qt1, v1);
+                if (resto1 != 0)
+                {
+                    printf("%d mesas com adultos+1: R$%d\n", qt3, v3);
+                }
+                printf("%d mesas com criancas: R$%d\n", qt2, v2);
+                if (resto2 != 0)
+                {
+                    printf("%d mesas com criancas+1: R$%d\n", qt4, v4);
+                }
+            }
+            else
+            {
+                qt1 = ((a - a % m) / m) + 1;
+                qt2 = ((c - c % m) / m) + 1;
+                v1 = qt1 * (3 * m);
+                v2 = qt2 * (2 * m);
                 printf("%d mesas com adultos: R$%d\n", qt1, v1);
                 printf("%d mesas com criancas: R$%d\n", qt2, v2);
-                printf("%d mesas com adultos+1: R$%d\n", qt3, v3);
-            }  
+            }
         }
     }
 }
