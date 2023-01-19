d0648736029a2c35d496eed76aaf0d5858e0b430
ftm2-cin <115846360+SENAC-Felipe@users.noreply.github.com>
2023-01-19 09:27:27 -0300

    dudinha

--- Listas/Lista 2/dudinha.c
+++ Listas/Lista 2/dudinha.c
@@ -2,6 +2,6 @@
 #include <math.h>
 
 int main() {
-    printf("Poxa duddcdcdinha, me desculpa, nao achei os numeros mas vou te mandar uma foto de um gatinho fofo.");
+    printf("Poxa dudinha, me desculpa, nao achei os numeros mas vou te mandar uma foto de um gatinho fofo.");
 	return 0;
 }
