9a2b832f42c95b049b9b1463f84853275e794e22
ftm2-cin <ftm2@cin.ufpe.br>
2023-01-31 16:58:30 -0300

    VETOR SORT

--- Aulas/vetor.c
+++ Aulas/vetor.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
-
+#define TAM 5
 int main()
 {
-    int v[] = {5, 6, 9, 13};
+    float vet[TAM] = {10, 9, -100, -2, 7};
+    int i, j, tmp;
+    int iMin = j;
+    for (j = 0; j < TAM; j++)
+    {
+        iMin = j;
+        for (i = j + 1; i < TAM; i++)
+        {
+            if (vet[i] < vet[iMin])
+            {
+                iMin = i;
+            }
+        }
+        if (iMin != j)
+        {
+            tmp = vet[j];
+            vet[j] = vet[iMin];
+            vet[iMin]= tmp;
+        }
+    }
+    printf("Menor elemento eh v[%d]=%.2f\n", iMin, vet[iMin]);
     return 0;
 }
