dc026f5d27923229f6aa86ffa620a4f9337a48ba
ftm2-cin <ftm2@cin.ufpe.br>
2023-02-02 16:50:13 -0300

    inicio bonxai

---
+++ Listas/Lista 3/bonxai.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    int x;
+    scanf("%d", &x);
+    char vetorC[x], letra;
+    for (int i = 0; i < x; i++)
+    {
+        scanf("%c ", vetorC[x]);
+    }
+    printf("\n");
+    
+    
+}
