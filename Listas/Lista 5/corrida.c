9d3b8cdcfe44d94fa6e36226a45dcd61fa8a9608
ftm2-cin <115846360+SENAC-Felipe@users.noreply.github.com>
2023-03-02 11:16:01 -0300

    corrida

--- Listas/Lista 5/corrida.c
+++ Listas/Lista 5/corrida.c
@@ -10,14 +10,62 @@ typedef struct
 
 int main()
 {
-    int qtd, money, i, j;
-    scanf("%d %d", &money, &qtd);
-    Peca pecas[qtd];
+    int qtd, money, i, j, k, cont = 0, rendimento = 0, valorT = 0, linha = 0, record = 0, posr = 0, matriz[50][3], dinheiros[50];
 
+    scanf("%d %d", &money, &qtd);
+
+    Peca pecas[qtd];
     for (i = 0; i < qtd; i++)
     {
-        fflush(stdin);
         scanf("%s %d %d", pecas[i].nome, &pecas[i].preco, &pecas[i].potencia);
+    }
+    for (i = 0; i < qtd; i++)
+    {
+        if (strcmp(pecas[i].nome, "Motor") == 0)
+        {
+            for (j = 0; j < qtd; j++)
+            {
+                if (strcmp(pecas[j].nome, "Roda") == 0)
+                {
+                    for (k = 0; k < qtd; k++)
+                    {
+                        if (strcmp(pecas[k].nome, "Lataria") == 0)
+                        {
+                            if (pecas[i].preco + pecas[j].preco + pecas[k].preco <= money)
+                            {
+                                matriz[linha][0] = pecas[i].potencia;
+                                matriz[linha][1] = pecas[j].potencia;
+                                matriz[linha][2] = pecas[k].potencia;
+                                dinheiros[linha] = pecas[i].preco + pecas[j].preco + pecas[k].preco;
+                                linha++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    for (i = 0; i < linha; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            rendimento += matriz[i][j];
+        }
+        if (rendimento > record)
+        {
+            record = rendimento;
+            posr = i;
+        }
+        rendimento = 0;
+    }
+    if (linha < 1 || qtd < 3)
+    {
+        printf("Nao foi possivel construir uma caranga...\n");
+    }
+    else
+    {
+        printf("Minha nave tem uma performance de %d pontos\n", record);
+        printf("E ainda me sobraram %d mangos!\n", money - dinheiros[posr]);
     }
 
     return 0;
